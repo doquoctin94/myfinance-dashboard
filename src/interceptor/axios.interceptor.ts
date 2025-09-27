febb5ee9539d8c9f9cf6b8916fbbc6249e2a5291
doquoctin94 <doquoctin94@gmail.com>
2025-09-27 15:39:08 +0700

    Cập nhật interceptor axios để xử lý lỗi 401 và 403, chuyển hướng đến trang đăng nhập khi nhận được mã lỗi tương ứng.

--- src/interceptor/axios.interceptor.ts
+++ src/interceptor/axios.interceptor.ts
@@ -8,7 +8,7 @@ axios.defaults.baseURL = 'https://api.myfiaivn.com/api/'
 axios.interceptors.response.use(response => {
   return response;
 }, error => {
-  if (error.response && error.response.status === 401) {
+  if (error.response && (error.response.status === 401 || error.response.status === 403)) {
     clearUserInfo();
     document.cookie = '';
     window.location.href = '/signin';
